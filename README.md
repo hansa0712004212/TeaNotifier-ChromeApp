47701fa7bc635b072c7c49d9ef4be5d0144349db
Hansa Amarasekara <hansa0712004212@gmail.com>
2016-05-29 19:31:12 +0530

    Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # TeaNotifier-ChromeApp
-Tea Notifier is my 1st Google Chrome extention. The purpose of this extention is to display notification for company members to inform tea time and let them to choose their preferred drink type.
+Tea Notifier is my 1st Google Chrome extention. 
+The purpose of this extention is to display notification for company members to inform tea time and let them to choose their preferred drink type.
+
+How to install.
+
+Open Google Chrome.
+Click on, "Customize and control google chrome" icon in right hand side top corner.
+Go to, "More Tools", select "Extensions".
+Drag and drop "TypefiTeaNotifier.crx" file in dist folder, onto Extensions view in Chrome.
+Click, "Add extension" button.
+
+
+If you are unable to see newly installed extension on toolbar,
+Go to "Apps",
+Drag and drop T Notifier icon into the toolbar.
---
+++ js/background.js
@@ -0,0 +1,157 @@
+/**
+ * Set morning tea time default hour.
+ * Value must be an integer between 0 - 11.
+ * @type Number
+ */
+var defaultMorningTHour = 10;
+
+/**
+ * Set morning tea time default minute.
+ * Value must be an integer between 0 - 59.
+ * @type Number
+ */
+var defaultMorningTMinute = 0;
+
+/**
+ * Set evening tea time default hour.
+ * Value must be an integer between 12 - 23.
+ * @type Number
+ */
+var defaultEveningTHour = 15;
+
+/**
+ * Set evening tea time default minute.
+ * Value must be an integer between 0 - 59.
+ * @type Number
+ */
+var defaultEveningTMinute = 30;
+
+/**
+ * Default text to speak out when notification pops up.
+ * @type String
+ */
+var defaultTTSText = "It is tea time. What is your choise ?";
+
+/**
+ * Default text to speech language. 
+ * en-IN for Indian English act.
+ * @type String
+ */
+var defaultTTSLanguage = "en-IN";
+
+/**
+ * Default text to speech speed. 
+ * 1.0 is any language default speed. 
+ * @type Number
+ */
+var defaultTTSSpeed = 0.8;
+
+// Global variables
+var date;
+var hours;
+var minutes;
+var seconds;
+var userEmail;
+
+/**
+ * Tick the time.
+ * @returns {undefined}
+ */
+var ticker = function () {
+    date = new Date();
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    seconds = date.getSeconds();
+    if (hours === defaultEveningTHour
+            && minutes === defaultEveningTMinute
+            && seconds === 00) {
+        notify();
+    }
+};
+
+/**
+ * Check time on each given interval.
+ * @param {type} function to execute
+ * @param {type} delay
+ */
+setInterval(ticker, 1000);
+
+/**
+ * Request permission on page load.
+ * @param {type} DOM content
+ * @param {type} function
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+});
+
+/**
+ * Get greeting message with current logged in user's email.
+ * @returns {String}
+ */
+var getUserEmailWithGreeting = function () {
+    var greeting = "Good Morning ";
+    if (hours > 12) {
+        greeting = "Good Evening ";
+    }
+    if (userEmail && userEmail !== null) {
+        return greeting + userEmail.split("@")[0];
+    } else {
+        return greeting + "Typefi User";
+    }
+};
+
+/**
+ * Get current signed in user's email address.
+ * @param {type} param
+ */
+chrome.identity.getProfileUserInfo(function (userInfo) {
+    userEmail = userInfo.email;
+});
+
+/**
+ * Launch notification.
+ * @returns {undefined}
+ */
+var notify = function () {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser.');
+        return;
+    }
+
+    if (Notification.permission !== "granted") {
+        Notification.requestPermission();
+    } else {
+        var notificationIcon = "images/morningTea.png";
+        if (hours > 11) {
+            notificationIcon = "images/eveningTea.png";
+        }
+        playNotificationSound();
+        var notification = new Notification(getUserEmailWithGreeting(),
+                {icon: notificationIcon,
+                    body: defaultTTSText});
+        setTimeout(platTTS, 1000);
+        notification.onclick = function () {
+            window.open("https://goo.gl/ghM5Md");
+        };
+    }
+};
+
+/**
+ * run TTS.
+ * @returns {undefined}
+ */
+var platTTS = function () {
+    chrome.tts.speak(defaultTTSText,
+            {'lang': defaultTTSLanguage, 'rate': defaultTTSSpeed});
+};
+
+/**
+ * Play notification sound.
+ * @returns {undefined}
+ */
+var playNotificationSound = function () {
+    var sound = new Audio('../sounds/popup.mp3');
+    sound.play();
+};
